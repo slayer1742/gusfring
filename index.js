f4fcb0536b700fe7b60ac64081f2d0fe5806281f
shadow0911 <yoyofahimmonaemkhandj@gmail.com>
2022-08-17 23:32:10 +0600

    first commit

--- index.js
+++ index.js
@@ -71,15 +71,12 @@ var fizzBuzz = function(n) {
         if(i % 3 == 0 && i % 5 == 0) {
             arr.push("FizzBuzz");
         }
-        
         else if(i % 3 == 0){
             arr.push("Fizz");
         }
-        
         else if(i % 5 == 0){
             arr.push("Buzz");
         }
-        
         else {
             arr.push(i);
         };
